3932248bcb039fc1e080616cc9a72c5b43122263
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2022-11-24 16:19:52 +0100

    file path seems working

--- main_ex.py
+++ main_ex.py
@@ -2,6 +2,7 @@ from pipeline_ex import ETL
 from analysis_ex import Analyser
 # "database_path": "C:\\Users\\user\\Dropbox\\DHBW Stuttgart\\Informatik 1 Python\\Projektarbeit_Wiessler"
 # figure_save_path = "C:\\Users\\user\\Dropbox\\DHBW Stuttgart\\Informatik 1 Python\\Projektarbeit_Wiessler\\venv\\vehicle_data_project_exercise\\"
+
 figure_save_path = "\\Users\\jacob\\data\\py\\22-1-04_programming_challenge"
 pipeline = ETL("data_specs.json")
 final_table = pipeline.run()
