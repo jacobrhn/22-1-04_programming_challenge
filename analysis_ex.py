dd88689704bff3c8061c76c52b8c35641e12effb
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2022-12-02 21:27:09 +0100

    implemented all def to create data_frames

--- analysis_ex.py
+++ analysis_ex.py
@@ -1,14 +1,10 @@
-import matplotlib.pyplot as plt
 import pandas as pd
-from datetime import datetime
-
 
 class DataFilter:
     def __init__(self, data_frame: pd.DataFrame, date_lower: str = None, date_upper: str = None):
         self.filtered_data = pd.DataFrame(data=data_frame)
         self.__date_lower = pd.to_datetime(date_lower, dayfirst=True)
         self.__date_upper = pd.to_datetime(date_upper, dayfirst=True)
-        # self.for_dates()
 
     def for_dates(self):
         for row in self.filtered_data.index:
@@ -27,40 +23,42 @@ class Analyser:
         self.visualize_sales_per_countries()
         self.visualize_sales_per_year()
 
-    def df_with_date_range(self, date_lower: str, date_upper: str):
+    def __df_with_date_range(self, date_lower: str, date_upper: str):
         filtered_data = DataFilter(data_frame=self.final_table, date_lower=date_lower, date_upper=date_upper)
         return filtered_data.for_dates()
 
     def df_sales_top_three_countries(self, date_lower: str, date_upper: str):
-        return self.df_with_date_range(date_lower=date_lower, date_upper=date_upper)\
-            .groupby(by=["country"]) \
-            .count().sort_values(by=["fin", "country"], ascending=False) \
-            .drop(columns=["fin", "production_date", "counter", "motor_type"]).iloc[[0, 1, 2]]
+        return self.__df_with_date_range(date_lower=date_lower, date_upper=date_upper) \
+            .groupby(by=["country"]).count()\
+            .sort_values(by=["fin", "country"], ascending=False)\
+            .drop(columns=["fin", "production_date", "motor_type"])\
+            .iloc[[0, 1, 2]]
 
     def df_sales_by_year(self, date_lower: str, date_upper: str):
-        date_filter = DataFilter(data_frame=self.final_table, date_lower=date_lower, date_upper=date_upper)
-        df_filtered_data = date_filter.for_dates()
-        df_filtered_data.loc[:, "production_year"] = pd.DatetimeIndex(df_filtered_data.loc[:, "production_date"]).year
-        return df_filtered_data.groupby(by="production_year").count().sort_values(by="counter", ascending=False)\
+        df_data_in_date_range = self.__df_with_date_range(date_lower=date_lower, date_upper=date_upper)
+        df_data_in_date_range.loc[:, "production_year"] = pd.DatetimeIndex(df_data_in_date_range.loc[:, "production_date"]).year
+        return df_data_in_date_range.groupby(by="production_year").count().sort_values(by="counter", ascending=False) \
             .drop(columns=["fin", "production_date", "country", "motor_type"])
 
     def df_fins_sorted_dates(self):
         return self.final_table.sort_values(by="production_date").drop(columns=["country", "counter", "motor_type"])
 
-    def df_vehicles_by_motor_types(self, date_lower: str, date_upper: str):
-        date_filter = DataFilter(data_frame=self.final_table, date_lower=date_lower, date_upper=date_upper)
-        df_filtered_data = date_filter.for_dates()
-        for row in df_filtered_data.index:
-            if not df_filtered_data.loc[row,"motor_type"] in ["OM 934", "OM 936", "OM 470", "OM 471"]:
-                df_filtered_data.drop(row, inplace=True)
-        return df_filtered_data.groupby(by="motor_type").count().drop(columns=["fin", "country", "production_date"])
-    def df_vehicles_by_motor_types_0(self, date_lower: str, date_upper: str):
-        date_filter = DataFilter(data_frame=self.final_table, date_lower=date_lower, date_upper=date_upper)
-        df_filtered_data = date_filter.for_dates()
-        for row in df_filtered_data.index:
-            if not df_filtered_data.loc[row,"motor_type"] in ["OM 934", "OM 936", "OM 470", "OM 471"]:
-                df_filtered_data.drop(row, inplace=True)
-        return df_filtered_data.groupby(by="motor_type").count().drop(columns=["fin", "country", "production_date"])
+    def df_vehicles_by_motor_types(self, date_lower: str, date_upper: str, motors_list: list):
+        df_data_in_date_range = self.__df_with_date_range(date_lower=date_lower, date_upper=date_upper)
+        for row in df_data_in_date_range.index:
+            if not df_data_in_date_range.loc[row, "motor_type"] in motors_list:
+                df_data_in_date_range.drop(row, inplace=True)
+        return df_data_in_date_range.groupby(by="motor_type").count().drop(columns=["fin", "country", "production_date"])
+
+    def df_vehicles_by_motor_types_country(self, date_lower: str, date_upper: str, motors_list: list, country: str):
+        df_data_in_date_range = self.__df_with_date_range(date_lower=date_lower, date_upper=date_upper)
+        for row in df_data_in_date_range.index:
+            if not df_data_in_date_range.loc[row, "motor_type"] in motors_list:
+                df_data_in_date_range.drop(row, inplace=True)
+        for row in df_data_in_date_range.index:
+            if not df_data_in_date_range.loc[row, "country"] == country:
+                df_data_in_date_range.drop(row, inplace=True)
+        return df_data_in_date_range["fin"]
 
     def filter_for_years(self):
         df = self.final_table
