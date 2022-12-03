fc5e12c3ff4493f8c76ae25d014d7a8f7f61e272
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2022-12-04 00:18:03 +0100

    final commit with `file_path`s leading to mr. wiesslers system

--- main_ex.py
+++ main_ex.py
@@ -1,12 +1,12 @@
 from pipeline_ex import ETL
 from analysis_ex import Analyser
 
-figure_save_path = "\\Users\\jacob\\data\\py\\22-1-04_programming_challenge"
+# figure_save_path = "\\Users\\jacob\\data\\py\\22-1-04_programming_challenge"
 # "database_path": "~/data/py/",
 # "database_path": "C:\\Users\\user\\Dropbox\\DHBW Stuttgart\\Informatik 1 Python\\Projektarbeit_Wiessler",
 
-# figure_save_path = "C:\\Users\\user\\Dropbox\\DHBW Stuttgart\\Informatik 1 " \
-                   #"Python\\Projektarbeit_Wiessler\\venv\\vehicle_data_project_exercise\\ "
+figure_save_path = "C:\\Users\\user\\Dropbox\\DHBW Stuttgart\\Informatik 1 " \
+                   "Python\\Projektarbeit_Wiessler\\venv\\vehicle_data_project_exercise\\ "
 pipeline = ETL("data_specs.json")
 final_table = pipeline.run()
 
