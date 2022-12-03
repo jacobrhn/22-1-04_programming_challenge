5af45c2230a6f74c86513590d83fa038342a4ddb
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2022-12-03 22:27:52 +0100

    implemented all tasks

--- analysis_ex.py
+++ analysis_ex.py
@@ -8,13 +8,6 @@ class Analyser:
         self.figure_save_path = figure_save_path
         self.final_table = table
 
-    def run(self):
-        self.visualize_sales_per_countries()
-        self.visualize_sales_per_year()
-
-    def show_results(self):
-        pass
-
     def filter_for_years(self, date_lower: str = None, date_upper: str = None):
         date_lower = pd.to_datetime(date_lower, dayfirst=True)
         date_upper = pd.to_datetime(date_upper, dayfirst=True)
@@ -25,29 +18,69 @@ class Analyser:
         return df_in_date_range.reset_index(drop=True)
 
     def df_sales_top_three_countries(self, date_lower: str, date_upper: str):
-        return self.filter_for_years(date_lower=date_lower, date_upper=date_upper) \
+        title = f"Top Three: sold vehicles per country "\
+                f"({pd.to_datetime(date_lower, dayfirst=True).year}-{pd.to_datetime(date_upper, dayfirst=True).year})"
+        df_filtered_grouped_counted = self.filter_for_years(date_lower=date_lower, date_upper=date_upper) \
             .groupby(by=["country"]).count()\
             .sort_values(by=["fin", "country"], ascending=False)\
             .drop(columns=["fin", "production_date", "motor_type"])\
             .iloc[[0, 1, 2]]
+        df_filtered_grouped_counted.plot(use_index=True, y=["counter"], kind="bar", color="gray")
+        plt.title(title)
+        plt.legend().set_visible(False)
+        plt.xlabel("Country")
+        plt.xticks(rotation=0)
+        plt.ylabel("Sold Vehicles (pc.)")
+        plt.grid(axis="y")
+        plt.savefig(self.figure_save_path + "countries.png")
+        print("________________________________________________________")
+        print(title)
+        print(df_filtered_grouped_counted)
 
     def df_sales_by_year(self, date_lower: str, date_upper: str):
+        title = f"Sold vehicles per year "\
+                f"({pd.to_datetime(date_lower, dayfirst=True).year}-{pd.to_datetime(date_upper, dayfirst=True).year})"
         df_in_date_range = self.filter_for_years(date_lower=date_lower, date_upper=date_upper)
         df_in_date_range.loc[:, "production_year"] = pd.DatetimeIndex(df_in_date_range.loc[:, "production_date"]).year
-        return df_in_date_range.groupby(by="production_year").count()\
+        df_filtered_grouped_counted =  df_in_date_range.groupby(by="production_year").count()\
             .drop(columns=["fin", "production_date", "country", "motor_type"])
-    def df_sales_by_year_sorted(self, date_lower: str, date_upper: str):
-        return self.df_sales_by_year(date_lower=date_lower, date_upper=date_upper).sort_values(by="counter", ascending=False)
+        df_filtered_grouped_counted.plot(use_index=True, color="gray", kind="bar")
+        plt.title(title)
+        plt.legend().set_visible(False)
+        plt.xlabel("Year")
+        plt.xticks(rotation=0)
+        plt.ylabel("Sold Vehicles (pc.)")
+        plt.grid(axis="y")
+        plt.savefig(self.figure_save_path + "years.png")
+        print("________________________________________________________")
+        print(title)
+        print(df_filtered_grouped_counted)
 
     def df_fins_sorted_dates(self):
-        return self.final_table.sort_values(by="production_date").drop(columns=["country", "counter", "motor_type"])
+        df_filtered = self.final_table.sort_values(by="production_date").drop(columns=["country", "counter", "motor_type"])
+        print("________________________________________________________")
+        print("First sold vehicle (FIN):")
+        print(df_filtered.iloc[0])
 
     def df_vehicles_by_motor_types(self, date_lower: str, date_upper: str, motors_list: list):
+        title = f"Sold vehicles per engine "\
+                f"({pd.to_datetime(date_lower, dayfirst=True).year}-{pd.to_datetime(date_upper, dayfirst=True).year})"
         df_in_date_range = self.filter_for_years(date_lower=date_lower, date_upper=date_upper)
         for row in df_in_date_range.index:
             if not df_in_date_range.loc[row, "motor_type"] in motors_list:
                 df_in_date_range.drop(row, inplace=True)
-        return df_in_date_range.groupby(by="motor_type").count().drop(columns=["fin", "country", "production_date"])
+        df_grouped_counted = df_in_date_range.groupby(by="motor_type").count().drop(columns=["fin", "country", "production_date"])
+        df_grouped_counted.plot(use_index=True, y=["counter"], kind="bar", color="gray")
+        plt.title(title)
+        plt.xlabel("Engine type")
+        plt.xticks(rotation=0)
+        plt.ylabel("Sold Vehicles (pc.)")
+        plt.legend().set_visible(False)
+        plt.grid(axis="y")
+        plt.savefig(self.figure_save_path + "engines.png")
+        print("________________________________________________________")
+        print(title)
+        print(df_grouped_counted)
 
     def df_vehicles_by_motor_types_country(self, date_lower: str, date_upper: str, motors_list: list, country: str):
         df_in_date_range = self.filter_for_years(date_lower=date_lower, date_upper=date_upper)
@@ -57,37 +90,7 @@ class Analyser:
         for row in df_in_date_range.index:
             if not df_in_date_range.loc[row, "country"] == country:
                 df_in_date_range.drop(row, inplace=True)
-        return df_in_date_range["fin"]
-
-    def graph_1(self, date_lower: str, date_upper: str):
-        figure_data = self.df_sales_top_three_countries(date_lower=date_lower, date_upper=date_upper)
-        figure_data.plot(use_index=True, y=["counter"], kind="bar", color="gray")
-        plt.title(f"Top Three: sold vehicles per country "
-                  f"({pd.to_datetime(date_lower, dayfirst=True).year}-"
-                  f"{pd.to_datetime(date_upper, dayfirst=True).year})")
-        plt.legend().set_visible(False)
-        plt.xlabel("Country")
-        plt.xticks(rotation=0)
-        plt.ylabel("Sold Vehicles (pc.)")
-        plt.savefig(self.figure_save_path + "countries.png")
-
-    def graph_2(self, date_lower: str, date_upper: str):
-        figure_data = self.df_sales_by_year(date_lower=date_lower, date_upper=date_upper)
-        figure_data.plot(use_index=True, color="gray", kind="bar")
-        plt.title(f"Sold vehicles per year "
-                  f"({pd.to_datetime(date_lower, dayfirst=True).year}-"
-                  f"{pd.to_datetime(date_upper, dayfirst=True).year})")
-        plt.legend().set_visible(False)
-        plt.xlabel("Year")
-        plt.xticks(rotation=0)
-        plt.ylabel("Sold Vehicles (pc.)")
-        plt.savefig(self.figure_save_path + "years.png")
-
-
-    def visualize_sales_per_countries(self):
-        # your code here #
-        pass
-
-    def visualize_sales_per_year(self):
-        # your code here #
-        pass
+        print("________________________________________________________")
+        print(f"Vehicles (FIN) with engine type {motors_list} and sold to {country} "\
+              f"({pd.to_datetime(date_lower, dayfirst=True).year}-{pd.to_datetime(date_upper, dayfirst=True).year})")
+        print(df_in_date_range["fin"])
